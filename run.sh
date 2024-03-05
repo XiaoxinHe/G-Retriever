4cee840c5f87bac3851e851f757003d30257586e
Cautiousss <862405237@qq.com>
2024-03-05 18:56:19 +0800

    fix: typo

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 for seed in 0 1 2 3 
 do
 # 1) inference only
+# a) Question-Only
+python inference.py --dataset expla_graphs --model_name inference_llm --llm_model_name 7b_chat --max_txt_len 0 --seed $seed
+python inference.py --dataset scene_graphs --model_name inference_llm --llm_model_name 7b_chat --max_txt_len 0 --seed $seed
+python inference.py --dataset webqsp --model_name inference_llm --llm_model_name 7b_chat --max_txt_len 0 --seed $seed
+# b) Textual Graph + Question
 python inference.py --dataset expla_graphs --model_name inference_llm --llm_model_name 7b_chat --seed $seed
 python inference.py --dataset scene_graphs --model_name inference_llm --llm_model_name 7b_chat --seed $seed
 python inference.py --dataset webqsp --model_name inference_llm --llm_model_name 7b_chat --seed $seed
 
 # 2) frozen llm + prompt tuning
-# a) query
-python train.py --dataset expla_graphs --model_name pt_llm --max_txt_len 0 --seed $seed
-python train.py --dataset scene_graphs_baseline --model_name pt_llm --max_txt_len 0 --seed $seed
-python train.py --dataset webqsp_baseline --model_name pt_llm --max_txt_len 0 --seed $seed
-
-# b) query + textual graph
+# a) prompt tuning
 python train.py --dataset expla_graphs --model_name pt_llm --seed $seed
 python train.py --dataset scene_graphs --model_name pt_llm --seed $seed
 python train.py --dataset webqsp --model_name pt_llm --seed $seed
-
-# c) g-retriever
+# b) g-retriever
 python train.py --dataset expla_graphs --model_name graph_llm --seed $seed
 python train.py --dataset scene_graphs --model_name graph_llm --seed $seed
 python train.py --dataset webqsp --model_name graph_llm --seed $seed
