7a66f83f1c6cb01946fef92411ad0b0ab96d8f17
XiaoxinHe <862405237@qq.com>
2024-05-14 11:03:03 +0000

    perf: use the huggingface Llama2 model checkpoints

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -4,16 +4,16 @@ from src.model.graph_llm import GraphLLM
 
 
 load_model = {
-    'llm': LLM,
-    'inference_llm': LLM,
-    'pt_llm': PromptTuningLLM,
-    'graph_llm': GraphLLM,
+    "llm": LLM,
+    "inference_llm": LLM,
+    "pt_llm": PromptTuningLLM,
+    "graph_llm": GraphLLM,
 }
 
 # Replace the following with the model paths
 llama_model_path = {
-    '7b': 'llama2/llama2_7b_hf',
-    '13b': 'llama2/llama2_13b_hf',
-    '7b_chat': 'llama2/llama2_7b_chat_hf',
-    '13b_chat': 'llama2/llama2_13b_chat_hf',
+    "7b": "meta-llama/Llama-2-7b-hf",
+    "7b_chat": "meta-llama/Llama-2-7b-chat-hf",
+    "13b": "meta-llama/Llama-2-13b-hf",
+    "13b_chat": "meta-llama/Llama-2-13b-chat-hf",
 }
